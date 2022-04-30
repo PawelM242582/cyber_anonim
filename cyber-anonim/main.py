885aacbca0f9063a3d0f19246884bb26beb188ac
P95 <48152248+PawelM242582@users.noreply.github.com>
2022-04-30 14:09:11 +0200

    Delete cyber-anonim directory

--- cyber-anonim/main.py
+++
@@ -1,8 +0,0 @@
-from datareader import dataread
-from dataClean import dataCleaning
-from anonim_ip import anonimizacion
-
-data = dataread()
-data = dataCleaning(data)
-data = anonimizacion(data)
-print(data)

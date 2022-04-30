6bead20cd30b4ae96787008cd80f7d2876ed305f
PawelM242582 <pawelekm2000@gmail.com>
2022-04-30 09:19:06 +0200

    clean nones

---
+++ cyber-anonim/dataClean.py
@@ -0,0 +1,12 @@
+
+def dataCleaning(data,pd):
+
+    nalist= data.columns[data.isna().any()].tolist()
+    typelist = data.dtypes
+
+    for item,typeitem in zip(nalist,typelist):
+        if typeitem in ('int64','float64'):
+            data[item] = data[item].fillna(0)
+        else:
+            data[item] = data[item].fillna('Lack')
+    return data

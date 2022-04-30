60685bdc6220d4f1bd3f320e039dcfd5c0076c7e
PawelM242582 <pawelekm2000@gmail.com>
2022-04-30 14:04:34 +0200

    change relative path

---
+++ cyber2/datareader.py
@@ -0,0 +1,16 @@
+def dataread():
+    import pandas as pd
+    try:
+
+
+        path = r'cyber2\UNSW_NB15\UNSW-NB15_1.csv'
+        label = r'cyber2\UNSW_NB15\NUSW-NB15_features.csv'
+        ng = pd.read_csv(label,low_memory=False,encoding_errors='ignore')
+        df = pd.read_csv(path,names=ng['Name'],low_memory=False,encoding_errors='ignore')
+
+        return df
+    except FileNotFoundError as NoSuchFile:
+        print(NoSuchFile)
+    except Exception as e:
+        print(e)
+
